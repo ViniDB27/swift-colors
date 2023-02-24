991b91320137d9042a0afd2685355c437afa7510
Vinicio Brejinski <viniciosdb@gmail.com>
2023-02-24 14:50:07 -0300

    Initial Commit

---
+++ colors/colorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorsApp.swift
+//  colors
+//
+//  Created by Vinicio Brejinski on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct colorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
